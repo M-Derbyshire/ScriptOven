b4c8b3699ed193f7792a73efa3f0fe4de694f095
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-10-15 22:33:36 +0100

    Moved the runFunctionOnInputText() function to its own file (it was in the runButtonOnClick file originally)

--- scripts/runButtonOnClick.js
+++ scripts/runButtonOnClick.js
@@ -25,31 +25,3 @@ function runButtonOnClick()
 	});
 	fileReader.readAsText(scriptFileInput.files[0]);
 }
-
-
-//Called by the FileReader callback (in runButtonOnClick()). Takes the 
-//text from the script file.
-function runFunctionOnInputText(functionText)
-{
-	//Does the given script text contain a "main" function that takes only 1 parameter?
-	if(!mainFunctionDefinitionIsCorrect(functionText))
-	{
-		handleError('The uploaded script does not contain a "main" function (or the given function does not accept only 1 parameter).');
-		return;
-	}
-	
-	const inTextInput = document.getElementById("inTextInput");
-	const outTextOuput = document.getElementById("outTextOuput");
-	
-	populateDynamicScript(functionText);
-	const resultText = main(inTextInput.value);
-	
-	//normal equality, so will also catch undefined
-	if(resultText == null)
-	{
-		handleError('The given "main" function ran, but it did not return a valid value.');
-		return;
-	}
-	
-	outTextOuput.value = resultText;
-}

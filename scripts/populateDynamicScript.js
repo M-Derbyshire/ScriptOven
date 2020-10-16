6eec07293d3b503537b394d0e47d89de034049aa
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-10-16 23:51:07 +0100

    solved the issue with the loading of new main scripts, after you've used another script file first. The populateDynamicScript function now replaces the script tag, rather than just repopulating the text of it

--- scripts/populateDynamicScript.js
+++ scripts/populateDynamicScript.js
@@ -1,5 +1,15 @@
 //Populates the dynamicScript element with the given script text
 function populateDynamicScript(scriptText)
 {
-	document.getElementById("dynamicScript").text = scriptText;
+	//If the user had selected a different script first, then just setting
+	//the text of the script tag won't change to the newer main() function. 
+	//We need to actually replace the script element.
+	const dynamicScriptElementID = "dynamicScript";
+	
+	const currentDynamicScript = document.getElementById(dynamicScriptElementID);
+	const newDynamicScript = document.createElement('script');
+	newDynamicScript.id = dynamicScriptElementID;
+	newDynamicScript.text = scriptText;
+	
+	currentDynamicScript.parentNode.replaceChild(newDynamicScript, currentDynamicScript);
 }

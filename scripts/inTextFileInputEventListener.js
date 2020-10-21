d8adb8588f3254c9f1cc2c4a859eff39b3c48b68
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-10-21 18:44:19 +0100

    removed the inline event attachment for the in-text file upload

--- scripts/inTextFileInputEventListener.js
+++ scripts/inTextFileInputEventListener.js
@@ -1,20 +1,23 @@
 //The event listener for the in-text file upload
-function inTextFileInputEventListener()
+function inTextFileInputEventListener(e)
 {
-	const fileInput = document.getElementById("inTextFileInput");
+	const fileInput = e.target;
 	const inTextArea = document.getElementById("inTextInput");
 	
 	//Get the file text and start the process
 	const fileReader = new FileReader();
-	fileReader.addEventListener('load', (event) => {
+	fileReader.onload = function(fileReadEvent)
+	{
 		try
 		{
-			inTextArea.value = event.target.result;
+			inTextArea.value = fileReadEvent.target.result;
 		}
 		catch(err)
 		{
 			handleError(err);
 		}
-	});
+	};
 	fileReader.readAsText(fileInput.files[0]);
 }
+
+document.getElementById("inTextFileInput").onchange = inTextFileInputEventListener;

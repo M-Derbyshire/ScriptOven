e1d7b2f44ccb9e2a2eb183cf8d532525ca59f9c0
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-10-21 21:00:10 +0100

    the download filename's input keyup event is no longer inline

--- scripts/downloadFilenameInputKeyUp.js
+++ scripts/downloadFilenameInputKeyUp.js
@@ -3,12 +3,14 @@
 //Better to use keyup, than onchange, to be sure that the
 //filename is always updated correctly, if focus is only lost
 //by clicking on the download button.
-function downloadFilenameInputKeyUp()
+function downloadFilenameInputKeyUp(e)
 {
-	const downloadFileNameInput = document.getElementById("downloadFileNameInput");
+	const downloadFileNameInput = e.target;
 	const outTextDownloadLink = document.getElementById("outTextDownloadLink");
 	const outTextDownloadButton = document.getElementById("outTextDownloadButton");
 	
 	outTextDownloadLink.download = downloadFileNameInput.value;
 	setDownloadButtonDisabledStatus();
 }
+
+document.getElementById("downloadFileNameInput").onkeyup = downloadFilenameInputKeyUp;

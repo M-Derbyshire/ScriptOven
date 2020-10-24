12a53a62d37c9bdd968f74291d7cda4923251c43
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-10-24 23:58:29 +0100

    populateDynamicScript will now wipe the three user functions before loading in the new script, as it was otherwise running the previous script's options function

--- scripts/populateDynamicScript.js
+++ scripts/populateDynamicScript.js
@@ -3,7 +3,15 @@ function populateDynamicScript(scriptText)
 {
 	//If the user had selected a different script first, then just setting
 	//the text of the script tag won't change to the newer main() function. 
-	//We need to actually replace the script element.
+	//We need to actually clear the functions and replace the script element.
+	
+	if(typeof main === typeof Function) main = null;
+	if(typeof options === typeof Function) options = null;
+	if(typeof assertions === typeof Function) assertions = null;
+	
+	//NOTE: If the user's script defines another object (rather than just the
+	//three that are cleared above), then that WILL NOT BE CLEARED.
+	
 	const dynamicScriptElementID = "dynamicScript";
 	
 	const currentDynamicScript = document.getElementById(dynamicScriptElementID);

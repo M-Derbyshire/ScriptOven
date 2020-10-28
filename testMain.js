4175b21b57c3158ac548b08b765a38a205d8a5a4
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-10-28 19:45:06 +0000

    setup testMain.js to be a good test of all functionality -- assertions/options/main (Also deleted testmain2.js, as wasn't really needed anymore).

--- testMain.js
+++ testMain.js
@@ -1,5 +1,48 @@
-function main(a)
+function main(inputString)
 {
-	console.log("main function run: " + a);
-	return "test output";
+	//Take the word "like", and replace it with "love".
+	//Also, if the last character in a value is a period, replace with an exclamation mark
+	
+	const inputLines = stringToLineArray(inputString);
+	const orderedHeaders = csvLineToStringArray(inputLines[0]);
+	let inputObjects = csvToObjectsArray(inputLines);
+	
+	inputObjects.forEach(obj => {
+		orderedHeaders.forEach(header => {
+			
+			const lastIndexInValue = obj[header].length - 1;
+			
+			obj[header] = obj[header].replace(/like/g, "love");
+			if(obj[header][lastIndexInValue] === '.')
+			{
+				obj[header] = obj[header].substring(0, lastIndexInValue) + "!";
+			}
+			
+		});
+	});
+	
+	const resultLines = objectsToCSVLinesArray(orderedHeaders, inputObjects);
+	return lineArrayToMultilineString(resultLines);
+}
+
+function assertions()
+{
+	return [
+		[
+			"column1,column2,column3\nI like dogs.,I like frogs.,I like hogs.\nI like cats,I like bats,I like rats",
+			"column1,column2,column3\nI love dogs!,I love frogs!,I love hogs!\nI love cats,I love bats,I love rats"
+		],
+		[
+			"column1,column2\nI like mice.,I like lice.\nI like birds,I like herds",
+			"column1,column2\nI love mice!,I love lice!\nI love birds,I love herds"
+		],
+	];
+}
+
+function options()
+{
+	return {
+		alertWhenAssertionsFailed: true,
+		consoleLogWhenAssertionsFailed: true
+	};
 }
